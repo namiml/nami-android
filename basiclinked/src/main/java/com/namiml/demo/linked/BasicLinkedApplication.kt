f62254e18454e4afec91e2a47548c18e025d5c66
Jigar Brahmbhatt <findjigar@gmail.com>
2020-02-29 16:28:09 -0500

    Make sure to start PaywallActivity with a NEW_TASK flag since we're starting it with application context

--- basiclinked/src/main/java/com/namiml/demo/linked/BasicLinkedApplication.kt
+++ basiclinked/src/main/java/com/namiml/demo/linked/BasicLinkedApplication.kt
@@ -1,8 +1,12 @@
 package com.namiml.demo.linked
 
 import android.app.Application
+import android.content.Intent
 import android.widget.Toast
-import com.namiml.*
+import com.namiml.Nami
+import com.namiml.NamiConfiguration
+import com.namiml.NamiLogLevel
+import com.namiml.NamiPaywallManager
 import com.namiml.entitlement.NamiEntitlement
 import com.namiml.entitlement.NamiEntitlementManager
 import com.namiml.entitlement.NamiEntitlementPlatform
@@ -12,26 +16,28 @@ class BasicLinkedApplication : Application() {
     companion object {
         private const val NAMI_APP_ID = "2dc699a5-43c6-4e3a-9166-957e1640741b"
     }
+
     override fun onCreate() {
         super.onCreate()
         Nami.configure(NamiConfiguration.build(this, NAMI_APP_ID) {
-                namiLogLevel = NamiLogLevel.DEBUG.takeIf { BuildConfig.DEBUG } ?: NamiLogLevel.ERROR
-            }
-        )
+            namiLogLevel = NamiLogLevel.DEBUG.takeIf { BuildConfig.DEBUG } ?: NamiLogLevel.ERROR
+        })
 
         NamiPaywallManager.registerApplicationSignInProvider { context, paywallData, developerPaywallID ->
             Toast.makeText(context, "Sign in clicked", Toast.LENGTH_SHORT).show()
         }
 
-        NamiPaywallManager.registerApplicationPaywallProvider { _, paywallData, products, developerPaywallId ->
+        NamiPaywallManager.registerApplicationPaywallProvider { context, paywallData, products, developerPaywallId ->
             val productsArray = ArrayList(products?.toMutableList() ?: mutableListOf())
             startActivity(
                 PaywallActivity.getIntent(
-                    this,
+                    context,
                     paywallData,
                     productsArray,
                     developerPaywallId
-                )
+                ).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
             )
         }
 
